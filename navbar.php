91855def217f944ac5ca16d71a740a25399dcc09
vic1997 <vic-du-68@live.fr>
2017-11-29 21:50:09 +0100

    Ajout de bootstrap et autres + modification de la navbar
    -Ajout de bootstrap,javascript et autres.
    -Modification de la avec les menus et le bouton pour la connexion.
    
    Il reste à faire que le bouton permet aussi l'inscription. Mais également que celui-ci ouvre un forms plus large (trop serré pour l'instant) sur PC (sur mobile pas de bug).

--- navbar.php
+++ navbar.php
@@ -1,24 +1,73 @@
 <html>
-<nav class="navbar navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarNav">
-  <ul class="navbar-nav">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Features</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Pricing</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link disabled" href="#">Disabled</a>
-    </li>
-  </ul>
-</div>
-</nav>
+    <link href="css/bootstrap.min.css" rel="stylesheet"> 
+        
+        <!--
+        <script src="js/jquery.js"></script>
+        <script src="js/popper/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        -->
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+    
+  
+  <div class="collapse navbar-collapse col" id="navbarNav">
+    <a class="navbar-brand" href="#">Navbar</a>
+      <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+        
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Recettes
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">1</a>
+          <a class="dropdown-item" href="#">2</a>
+        </div>
+      </li>
+        
+        <li class="nav-item">
+        <a class="nav-link" href="#">Planning</a>
+      </li>
+    
+    </ul>
+    </div>
+    
+    <div class="dropdown col">
+  <button class="btn btn-light dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Se connecter
+  </button>
+        
+  <form class="dropdown-menu p-4">
+  <div class="form-group">
+    <label for="exampleDropdownFormEmail2">Adresse e-mail</label>
+    <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleDropdownFormPassword2">Mot de passe</label>
+    <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <label class="form-check-label">
+      <input type="checkbox" class="form-check-input">
+      Retenir mot de passe
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Connexion</button>
+</form>
+</div>  
+      
+      
+  </div>
+    </nav>
 </html>
