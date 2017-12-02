6dc084893502f9919b17aa75082f6d8b06474736
vic1997 <vic-du-68@live.fr>
2017-12-02 19:49:03 +0100

    Correction de la barre de naviguation
    - Debug de la barre en version mobile et pc (peut pas faire mieux)
    - Changement des espaces entre les boutons
    - Changement de couleur pour s'inscrire
    - Modification du panneau pour s'inscrire
    - Ajout d'un logo
    - Modification des sous-menu pour Recettes

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <html>
-    <link href="css/bootstrap.min.css" rel="stylesheet"> 
-        
+    <link href="css/bootstrap.css" rel="stylesheet"> 
+    
         <!--
         <script src="js/jquery.js"></script>
         <script src="js/popper/popper.min.js"></script>
@@ -13,6 +13,9 @@
     
     
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
+    <a class="navbar-brand" href="#">
+    <img src="https://recettes-et-mirettes.fr/wp-content/uploads/2017/09/09.png" width="30" height="30" alt="">
+    </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -20,7 +23,7 @@
     
   
   <div class="collapse navbar-collapse col" id="navbarNav">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Site de Recettes</a>
       <ul class="navbar-nav">
       <li class="nav-item active">
         <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
@@ -31,8 +34,8 @@
           Recettes
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">1</a>
-          <a class="dropdown-item" href="#">2</a>
+          <a class="dropdown-item" href="#">Ajouter une recette</a>
+          <a class="dropdown-item" href="#">Liste des recettes</a>
         </div>
       </li>
         
@@ -43,31 +46,59 @@
     </ul>
     </div>
     
-    <div class="dropdown col">
-  <button class="btn btn-light dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Se connecter
-  </button>
-        
-  <form class="dropdown-menu p-4">
-  <div class="form-group">
-    <label for="exampleDropdownFormEmail2">Adresse e-mail</label>
-    <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
-  </div>
-  <div class="form-group">
-    <label for="exampleDropdownFormPassword2">Mot de passe</label>
-    <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
-  </div>
-  <div class="form-check">
-    <label class="form-check-label">
-      <input type="checkbox" class="form-check-input">
-      Retenir mot de passe
-    </label>
-  </div>
-  <button type="submit" class="btn btn-primary">Connexion</button>
-</form>
-</div>  
-      
-      
-  </div>
+    
+    <div class="container col">
+
+
+        <div class="dropdown col"> <!-- Dropdown menu connecter -->
+      <button class="btn btn-light dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Se connecter
+      </button>
+      <form class="dropdown-menu pull-left p-4">
+      <div class="form-group">
+        <label for="exampleDropdownFormEmail2">Adresse e-mail</label>
+        <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
+      </div>
+      <div class="form-group">
+        <label for="exampleDropdownFormPassword2">Mot de passe</label>
+        <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
+      </div>
+      <div class="form-check">
+        <label class="form-check-label">
+          <input type="checkbox" class="form-check-input">
+          Retenir mot de passe
+        </label>
+      </div>
+      <button type="submit" class="btn btn-primary">Connexion</button>
+    </form>
+    </div>  
+
+
+
+
+
+
+        <div class="dropdown col"> <!--dropdown menu pour s'inscrire-->
+      <button class="btn btn-primary dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        S'inscrire
+      </button>
+      <form class="dropdown-menu pull-left p-4">
+      <div class="form-group">
+        <label for="exampleDropdownFormEmail2">Adresse e-mail</label>
+        <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
+      </div>
+      <div class="form-group">
+        <label for="exampleDropdownFormPassword2">Mot de passe</label>
+        <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
+      </div>
+      <button type="submit" class="btn btn-primary">S'inscrire</button>
+    </form>
+    </div>
+    
+    <div class="container col-7">
+        </div>
+    
+    </div>
+  
     </nav>
 </html>
