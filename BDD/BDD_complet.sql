c5c3d731b429db0e5e33cb14f179c15deb92498e
Unknown <thespecialone68@hotmail.fr>
2017-12-18 22:26:47 +0100

    MAJ script complet BDD

--- BDD/BDD_complet.sql
+++ BDD/BDD_complet.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.3
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 01 déc. 2017 à 19:57
--- Version du serveur :  5.7.19
--- Version de PHP :  5.6.31
+-- Hôte : front-ha-mysql-01.shpv.fr:3306
+-- Généré le :  lun. 18 déc. 2017 à 22:23
+-- Version du serveur :  5.6.38
+-- Version de PHP :  5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,8 +19,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `cuisine`
+-- Base de données :  `wiyhousp_recettes`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `administration`
+--
+
+CREATE TABLE `administration` (
+  `id` int(11) NOT NULL,
+  `login` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `mdp` varchar(100) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `administration`
+--
+
+INSERT INTO `administration` (`id`, `login`, `mdp`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -28,13 +47,10 @@ SET time_zone = "+00:00";
 -- Structure de la table `conforme`
 --
 
-DROP TABLE IF EXISTS `conforme`;
-CREATE TABLE IF NOT EXISTS `conforme` (
+CREATE TABLE `conforme` (
   `OuiNon` tinyint(1) NOT NULL,
   `idRec` int(11) NOT NULL,
-  `idReg` int(11) NOT NULL,
-  PRIMARY KEY (`idRec`,`idReg`),
-  KEY `FK_conforme_idReg` (`idReg`)
+  `idReg` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -45,12 +61,12 @@ INSERT INTO `conforme` (`OuiNon`, `idRec`, `idReg`) VALUES
 (1, 1, 1),
 (0, 1, 2),
 (0, 1, 3),
-(1, 2, 1),
-(1, 2, 2),
-(1, 2, 3),
 (0, 3, 1),
 (0, 3, 2),
-(0, 3, 3);
+(0, 3, 3),
+(1, 8, 1),
+(1, 8, 2),
+(1, 8, 3);
 
 -- --------------------------------------------------------
 
@@ -58,13 +74,10 @@ INSERT INTO `conforme` (`OuiNon`, `idRec`, `idReg`) VALUES
 -- Structure de la table `contenu`
 --
 
-DROP TABLE IF EXISTS `contenu`;
-CREATE TABLE IF NOT EXISTS `contenu` (
+CREATE TABLE `contenu` (
   `quantite` float NOT NULL,
   `idRec` int(11) NOT NULL,
-  `idIngre` int(11) NOT NULL,
-  PRIMARY KEY (`idRec`,`idIngre`),
-  KEY `FK_contenu_idIngre` (`idIngre`)
+  `idIngre` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -79,14 +92,6 @@ INSERT INTO `contenu` (`quantite`, `idRec`, `idIngre`) VALUES
 (3, 1, 5),
 (1, 1, 6),
 (100, 1, 7),
-(1, 2, 1),
-(100, 2, 8),
-(0.5, 2, 9),
-(2, 2, 10),
-(20, 2, 11),
-(1, 2, 12),
-(50, 2, 13),
-(20, 2, 14),
 (50, 3, 1),
 (50, 3, 7),
 (6, 3, 15),
@@ -96,7 +101,51 @@ INSERT INTO `contenu` (`quantite`, `idRec`, `idIngre`) VALUES
 (10, 3, 19),
 (20, 3, 20),
 (20, 3, 21),
-(10, 3, 22);
+(10, 3, 22),
+(1, 8, 1),
+(100, 8, 8),
+(0.5, 8, 9),
+(2, 8, 10),
+(20, 8, 11),
+(1, 8, 12),
+(50, 8, 13),
+(20, 8, 14);
+
+--
+-- Déclencheurs `contenu`
+--
+DELIMITER $$
+CREATE TRIGGER `majNutrition` AFTER INSERT ON `contenu` FOR EACH ROW BEGIN
+	DECLARE qteCal FLOAT;
+    DECLARE qteProte FLOAT;
+    DECLARE qteGlu FLOAT;
+    DECLARE qteLip FLOAT;
+    DECLARE qteProti FLOAT;
+    	
+	SET qteCal = (SELECT qteCalories FROM ingredient WHERE ingredient.idIngre = NEW.idIngre);
+    SET qteProte = (SELECT qteProteines FROM ingredient WHERE ingredient.idIngre = NEW.idIngre);
+    SET qteGlu = (SELECT qteGlucides FROM ingredient WHERE ingredient.idIngre = NEW.idIngre);
+    SET qteLip = (SELECT qteLipides FROM ingredient WHERE ingredient.idIngre = NEW.idIngre);
+    SET qteProti = (SELECT qteProtides FROM ingredient WHERE ingredient.idIngre = NEW.idIngre);
+	
+	UPDATE recette
+	SET qteCalories = qteCalories+(qteCal*NEW.quantite)
+	WHERE idRec = NEW.idRec;
+    UPDATE recette
+	SET qteProteines = qteProteines+(qteProte*NEW.quantite)
+	WHERE idRec = NEW.idRec;
+    UPDATE recette
+	SET qteGlucides = qteGlucides+(qteGlu*NEW.quantite)
+	WHERE idRec = NEW.idRec;
+    UPDATE recette
+	SET qteLipides = qteLipides+(qteLip*NEW.quantite)
+	WHERE idRec = NEW.idRec;
+    UPDATE recette
+	SET qteProtides = qteProtides+(qteProti*NEW.quantite)
+	WHERE idRec = NEW.idRec;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -104,13 +153,10 @@ INSERT INTO `contenu` (`quantite`, `idRec`, `idIngre`) VALUES
 -- Structure de la table `contient`
 --
 
-DROP TABLE IF EXISTS `contient`;
-CREATE TABLE IF NOT EXISTS `contient` (
+CREATE TABLE `contient` (
   `quantite` int(11) DEFAULT NULL,
   `idListe` int(11) NOT NULL,
-  `idIngre` int(11) NOT NULL,
-  PRIMARY KEY (`idListe`,`idIngre`),
-  KEY `FK_contient_idIngre` (`idIngre`)
+  `idIngre` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -126,12 +172,9 @@ INSERT INTO `contient` (`quantite`, `idListe`, `idIngre`) VALUES
 -- Structure de la table `est_present`
 --
 
-DROP TABLE IF EXISTS `est_present`;
-CREATE TABLE IF NOT EXISTS `est_present` (
+CREATE TABLE `est_present` (
   `idMen` int(11) NOT NULL,
-  `idRec` int(11) NOT NULL,
-  PRIMARY KEY (`idMen`,`idRec`),
-  KEY `FK_est_present_idRec` (`idRec`)
+  `idRec` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -141,9 +184,9 @@ CREATE TABLE IF NOT EXISTS `est_present` (
 INSERT INTO `est_present` (`idMen`, `idRec`) VALUES
 (1, 1),
 (2, 1),
-(1, 2),
-(2, 2),
-(1, 3);
+(1, 3),
+(1, 8),
+(2, 8);
 
 -- --------------------------------------------------------
 
@@ -151,13 +194,10 @@ INSERT INTO `est_present` (`idMen`, `idRec`) VALUES
 -- Structure de la table `est_servi`
 --
 
-DROP TABLE IF EXISTS `est_servi`;
-CREATE TABLE IF NOT EXISTS `est_servi` (
+CREATE TABLE `est_servi` (
   `idMen` int(11) NOT NULL,
   `idPlan` int(11) NOT NULL,
-  `dateServi` date NOT NULL,
-  PRIMARY KEY (`idMen`,`idPlan`,`dateServi`),
-  KEY `FK_est_servi_idPlan` (`idPlan`)
+  `dateServi` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -176,14 +216,11 @@ INSERT INTO `est_servi` (`idMen`, `idPlan`, `dateServi`) VALUES
 -- Structure de la table `illustration`
 --
 
-DROP TABLE IF EXISTS `illustration`;
-CREATE TABLE IF NOT EXISTS `illustration` (
-  `idIllu` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `illustration` (
+  `idIllu` int(11) NOT NULL,
   `adresse` varchar(1000) NOT NULL,
-  `idRec` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idIllu`),
-  KEY `FK_ILLUSTRATION_idRec` (`idRec`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `idRec` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `illustration`
@@ -191,8 +228,8 @@ CREATE TABLE IF NOT EXISTS `illustration` (
 
 INSERT INTO `illustration` (`idIllu`, `adresse`, `idRec`) VALUES
 (1, 'http://www.cavevieilarmand.com/imagesSite/67.jpg', 1),
-(2, 'https://image.afcdn.com/recipe/20160425/1491_w420h344c1cx2000cy3000.jpg', 2),
-(3, 'https://image.afcdn.com/recipe/20131102/55590_w420h344c1cx265cy121.jpg', 3);
+(3, 'https://image.afcdn.com/recipe/20131102/55590_w420h344c1cx265cy121.jpg', 3),
+(4, 'https://image.afcdn.com/recipe/20160425/1491_w420h344c1cx2000cy3000.jpg', 8);
 
 -- --------------------------------------------------------
 
@@ -200,9 +237,8 @@ INSERT INTO `illustration` (`idIllu`, `adresse`, `idRec`) VALUES
 -- Structure de la table `ingredient`
 --
 
-DROP TABLE IF EXISTS `ingredient`;
-CREATE TABLE IF NOT EXISTS `ingredient` (
-  `idIngre` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `ingredient` (
+  `idIngre` int(11) NOT NULL,
   `nom` varchar(25) NOT NULL,
   `mesure` varchar(25) NOT NULL,
   `qteParDefaut` int(11) NOT NULL,
@@ -210,9 +246,8 @@ CREATE TABLE IF NOT EXISTS `ingredient` (
   `qteProteines` float NOT NULL,
   `qteGlucides` float NOT NULL,
   `qteLipides` float NOT NULL,
-  `qteProtides` float NOT NULL,
-  PRIMARY KEY (`idIngre`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+  `qteProtides` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `ingredient`
@@ -248,13 +283,10 @@ INSERT INTO `ingredient` (`idIngre`, `nom`, `mesure`, `qteParDefaut`, `qteCalori
 -- Structure de la table `interdit`
 --
 
-DROP TABLE IF EXISTS `interdit`;
-CREATE TABLE IF NOT EXISTS `interdit` (
+CREATE TABLE `interdit` (
   `OuiNon` tinyint(1) NOT NULL,
   `idReg` int(11) NOT NULL,
-  `idIngre` int(11) NOT NULL,
-  PRIMARY KEY (`idReg`,`idIngre`),
-  KEY `FK_interdit_idIngre` (`idIngre`)
+  `idIngre` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -335,15 +367,12 @@ INSERT INTO `interdit` (`OuiNon`, `idReg`, `idIngre`) VALUES
 -- Structure de la table `liste_achat`
 --
 
-DROP TABLE IF EXISTS `liste_achat`;
-CREATE TABLE IF NOT EXISTS `liste_achat` (
-  `idListe` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `liste_achat` (
+  `idListe` int(11) NOT NULL,
   `datePrevue` date NOT NULL,
   `archiveON` tinyint(1) NOT NULL,
-  `idUtil` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idListe`),
-  KEY `FK_LISTE_ACHAT_idUtil` (`idUtil`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `idUtil` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `liste_achat`
@@ -358,13 +387,10 @@ INSERT INTO `liste_achat` (`idListe`, `datePrevue`, `archiveON`, `idUtil`) VALUE
 -- Structure de la table `menu`
 --
 
-DROP TABLE IF EXISTS `menu`;
-CREATE TABLE IF NOT EXISTS `menu` (
-  `idMen` int(11) NOT NULL AUTO_INCREMENT,
-  `idUtil` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idMen`),
-  KEY `FK_MENU_idUtil` (`idUtil`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+CREATE TABLE `menu` (
+  `idMen` int(11) NOT NULL,
+  `idUtil` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `menu`
@@ -380,14 +406,10 @@ INSERT INTO `menu` (`idMen`, `idUtil`) VALUES
 -- Structure de la table `peut_remplacer`
 --
 
-DROP TABLE IF EXISTS `peut_remplacer`;
-CREATE TABLE IF NOT EXISTS `peut_remplacer` (
+CREATE TABLE `peut_remplacer` (
   `idIngre` int(11) NOT NULL,
   `idIngre_INGREDIENT` int(11) NOT NULL,
-  `idRec` int(11) NOT NULL,
-  PRIMARY KEY (`idIngre`,`idIngre_INGREDIENT`,`idRec`),
-  KEY `FK_peut_remplacer_idIngre_INGREDIENT` (`idIngre_INGREDIENT`),
-  KEY `FK_peut_remplacer_idRec` (`idRec`)
+  `idRec` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -403,16 +425,13 @@ INSERT INTO `peut_remplacer` (`idIngre`, `idIngre_INGREDIENT`, `idRec`) VALUES
 -- Structure de la table `planning`
 --
 
-DROP TABLE IF EXISTS `planning`;
-CREATE TABLE IF NOT EXISTS `planning` (
-  `idPlan` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `planning` (
+  `idPlan` int(11) NOT NULL,
   `dateDebut` date NOT NULL,
   `dateFin` date NOT NULL,
   `archiveON` tinyint(1) DEFAULT NULL,
-  `idUtil` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idPlan`),
-  KEY `FK_PLANNING_idUtil` (`idUtil`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `idUtil` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `planning`
@@ -428,13 +447,10 @@ INSERT INTO `planning` (`idPlan`, `dateDebut`, `dateFin`, `archiveON`, `idUtil`)
 -- Structure de la table `possede`
 --
 
-DROP TABLE IF EXISTS `possede`;
-CREATE TABLE IF NOT EXISTS `possede` (
+CREATE TABLE `possede` (
   `quantite` int(11) DEFAULT NULL,
   `idIngre` int(11) NOT NULL,
-  `idUtil` int(11) NOT NULL,
-  PRIMARY KEY (`idIngre`,`idUtil`),
-  KEY `FK_possede_idUtil` (`idUtil`)
+  `idUtil` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -471,9 +487,8 @@ INSERT INTO `possede` (`quantite`, `idIngre`, `idUtil`) VALUES
 -- Structure de la table `recette`
 --
 
-DROP TABLE IF EXISTS `recette`;
-CREATE TABLE IF NOT EXISTS `recette` (
-  `idRec` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `recette` (
+  `idRec` int(11) NOT NULL,
   `nom` varchar(100) NOT NULL,
   `descriptif` varchar(10000) NOT NULL,
   `difficulte` varchar(25) NOT NULL,
@@ -487,10 +502,8 @@ CREATE TABLE IF NOT EXISTS `recette` (
   `qteGlucides` float NOT NULL,
   `qteLipides` float NOT NULL,
   `qteProtides` float NOT NULL,
-  `idUtil` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idRec`),
-  KEY `FK_RECETTE_idUtil` (`idUtil`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `idUtil` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `recette`
@@ -498,8 +511,8 @@ CREATE TABLE IF NOT EXISTS `recette` (
 
 INSERT INTO `recette` (`idRec`, `nom`, `descriptif`, `difficulte`, `prix`, `nbPersonnes`, `dureePreparation`, `dureeCuisson`, `dureeTotale`, `qteCalories`, `qteProteines`, `qteGlucides`, `qteLipides`, `qteProtides`, `idUtil`) VALUES
 (1, 'Kougelhopf', 'Gâteau alsacien', 'Facile', 1, 8, 10, 45, 55, 735, 735, 735, 735, 735, 1),
-(2, 'Salade de quinoa', 'Ca ressemble a du taboulé, mais c\'est beaucoup plus léger, et vraiment plus original !! Ca croustille!!!', 'Très facile', 1, 4, 15, 15, 30, 194.5, 194.5, 194.5, 194.5, 194.5, 2),
-(3, 'Langue de boeuf sauce piquante aux cornichons', 'Ce plat est encore meilleur réchauffé.', 'Difficile', 3, 6, 30, 105, 135, 787, 787, 787, 787, 787, 1);
+(3, 'Langue de boeuf sauce piquante aux cornichons', 'Ce plat est encore meilleur réchauffé.', 'Difficile', 3, 6, 30, 105, 135, 787, 787, 787, 787, 787, 1),
+(8, 'Salade de quinoa', 'Ca ressemble a du taboulé, mais c\'est beaucoup plus léger, et vraiment plus original !! Ca croustille!!!', 'Très facile', 1, 4, 15, 15, 30, 194.5, 194.5, 194.5, 194.5, 194.5, 2);
 
 -- --------------------------------------------------------
 
@@ -507,13 +520,10 @@ INSERT INTO `recette` (`idRec`, `nom`, `descriptif`, `difficulte`, `prix`, `nbPe
 -- Structure de la table `regime`
 --
 
-DROP TABLE IF EXISTS `regime`;
-CREATE TABLE IF NOT EXISTS `regime` (
-  `idReg` int(11) NOT NULL AUTO_INCREMENT,
-  `libelle` varchar(25) NOT NULL,
-  PRIMARY KEY (`idReg`),
-  UNIQUE KEY `libelle` (`libelle`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+CREATE TABLE `regime` (
+  `idReg` int(11) NOT NULL,
+  `libelle` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `regime`
@@ -530,26 +540,199 @@ INSERT INTO `regime` (`idReg`, `libelle`) VALUES
 -- Structure de la table `utilisateur`
 --
 
-DROP TABLE IF EXISTS `utilisateur`;
-CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `idUtil` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `utilisateur` (
+  `idUtil` int(11) NOT NULL,
   `nom` varchar(25) NOT NULL,
   `prenom` varchar(25) NOT NULL,
   `login` varchar(25) NOT NULL,
   `mail` varchar(25) NOT NULL,
-  `mdp` varchar(25) NOT NULL,
-  PRIMARY KEY (`idUtil`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `mdp` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`idUtil`, `nom`, `prenom`, `login`, `mail`, `mdp`) VALUES
-(1, 'Kanawati', 'Samy', 'admin', 'test@gmail.com', 'admin'),
-(2, 'Razowski', 'Bob', 'bob', 'bob@gmail.com', 'bob'),
-(3, 'De Santa', 'Michael', 'mds', 'mds@hotmail.fr', 'mds');
+(1, 'Administrateur', '', 'test', 'test@gmail.com', '098f6bcd4621d373cade4e832627b4f6'),
+(2, 'Razowski', 'Bob', 'bob', 'bob@gmail.com', '9f9d51bc70ef21ca5c14f307980a29d8'),
+(3, 'De Santa', 'Michael', 'mds', 'mds@hotmail.fr', '60a114c91c41983174b484e188856fb3'),
+(6, 'Coffe', 'Jean-Pierre', 'Coffe', 'jpcoffe@ripdouille.fr', '8860aab6453e117ddbc7a373fcd1eb68'),
+(12, 'renard', 'gilbert', 'guillaume', 'g@arte.tv', '098f6bcd4621d373cade4e832627b4f6');
 
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `administration`
+--
+ALTER TABLE `administration`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- Index pour la table `conforme`
+--
+ALTER TABLE `conforme`
+  ADD PRIMARY KEY (`idRec`,`idReg`),
+  ADD KEY `FK_conforme_idReg` (`idReg`);
+
+--
+-- Index pour la table `contenu`
+--
+ALTER TABLE `contenu`
+  ADD PRIMARY KEY (`idRec`,`idIngre`),
+  ADD KEY `FK_contenu_idIngre` (`idIngre`);
+
+--
+-- Index pour la table `contient`
+--
+ALTER TABLE `contient`
+  ADD PRIMARY KEY (`idListe`,`idIngre`),
+  ADD KEY `FK_contient_idIngre` (`idIngre`);
+
+--
+-- Index pour la table `est_present`
+--
+ALTER TABLE `est_present`
+  ADD PRIMARY KEY (`idMen`,`idRec`),
+  ADD KEY `FK_est_present_idRec` (`idRec`);
+
+--
+-- Index pour la table `est_servi`
+--
+ALTER TABLE `est_servi`
+  ADD PRIMARY KEY (`idMen`,`idPlan`,`dateServi`),
+  ADD KEY `FK_est_servi_idPlan` (`idPlan`);
+
+--
+-- Index pour la table `illustration`
+--
+ALTER TABLE `illustration`
+  ADD PRIMARY KEY (`idIllu`),
+  ADD KEY `FK_ILLUSTRATION_idRec` (`idRec`);
+
+--
+-- Index pour la table `ingredient`
+--
+ALTER TABLE `ingredient`
+  ADD PRIMARY KEY (`idIngre`),
+  ADD UNIQUE KEY `nom` (`nom`);
+
+--
+-- Index pour la table `interdit`
+--
+ALTER TABLE `interdit`
+  ADD PRIMARY KEY (`idReg`,`idIngre`),
+  ADD KEY `FK_interdit_idIngre` (`idIngre`);
+
+--
+-- Index pour la table `liste_achat`
+--
+ALTER TABLE `liste_achat`
+  ADD PRIMARY KEY (`idListe`),
+  ADD KEY `FK_LISTE_ACHAT_idUtil` (`idUtil`);
+
+--
+-- Index pour la table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`idMen`),
+  ADD KEY `FK_MENU_idUtil` (`idUtil`);
+
+--
+-- Index pour la table `peut_remplacer`
+--
+ALTER TABLE `peut_remplacer`
+  ADD PRIMARY KEY (`idIngre`,`idIngre_INGREDIENT`,`idRec`),
+  ADD KEY `FK_peut_remplacer_idIngre_INGREDIENT` (`idIngre_INGREDIENT`),
+  ADD KEY `FK_peut_remplacer_idRec` (`idRec`);
+
+--
+-- Index pour la table `planning`
+--
+ALTER TABLE `planning`
+  ADD PRIMARY KEY (`idPlan`),
+  ADD KEY `FK_PLANNING_idUtil` (`idUtil`);
+
+--
+-- Index pour la table `possede`
+--
+ALTER TABLE `possede`
+  ADD PRIMARY KEY (`idIngre`,`idUtil`),
+  ADD KEY `FK_possede_idUtil` (`idUtil`);
+
+--
+-- Index pour la table `recette`
+--
+ALTER TABLE `recette`
+  ADD PRIMARY KEY (`idRec`),
+  ADD KEY `FK_RECETTE_idUtil` (`idUtil`);
+
+--
+-- Index pour la table `regime`
+--
+ALTER TABLE `regime`
+  ADD PRIMARY KEY (`idReg`),
+  ADD UNIQUE KEY `libelle` (`libelle`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`idUtil`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `administration`
+--
+ALTER TABLE `administration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `illustration`
+--
+ALTER TABLE `illustration`
+  MODIFY `idIllu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT pour la table `ingredient`
+--
+ALTER TABLE `ingredient`
+  MODIFY `idIngre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+--
+-- AUTO_INCREMENT pour la table `liste_achat`
+--
+ALTER TABLE `liste_achat`
+  MODIFY `idListe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `idMen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `planning`
+--
+ALTER TABLE `planning`
+  MODIFY `idPlan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `recette`
+--
+ALTER TABLE `recette`
+  MODIFY `idRec` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT pour la table `regime`
+--
+ALTER TABLE `regime`
+  MODIFY `idReg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `idUtil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- Contraintes pour les tables déchargées
 --
